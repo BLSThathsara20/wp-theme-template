3a01be833062eafff5fb1d01f02ac35c8735eef8
blsthathsara <savindu.thathsara@enfection.com>
2022-08-19 10:33:40 +0530

    First commit Signed-off-by: blsthathsara <savindu.thathsara@enfection.com>

---
+++ template-parts/home-template.php
@@ -0,0 +1,353 @@
+<?php 
+/** 
+* Template Name: Home Page
+*
+* @package base_theme
+**/ 
+get_header();
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+?>
+
+<div class="page-content home-page">
+    <section class="hero-section">
+        <div class="hero-wrapper"
+            style="background-image: url('http://localhost/projects/techventuras/wp-content/uploads/2022/03/banner.jpg')">
+            <div class="container">
+                <!-- hero content starts here -->
+                <div class="hero-content">
+                    <h2 class="hero-title">We Build Digitally-Inclusive
+                        Economies</h2>
+                    <div class="sub-title">
+                        <img src="http://localhost/projects/techventuras/wp-content/uploads/2022/03/play_circle_filled_24px_outlined.svg"
+                            alt="">
+                        <h5 class="hero-redline">Empowering Livelihoods Together</h5>
+                    </div>
+                    <div class="hero-paragraph">
+                        <p>Our innovative solutions have a direct impact on human experiences and improve digital
+                            literacy in communities.</p>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="btn homepage-btn hvr-grow">
+                            <a href="<?php //the_field('header_button', 'option'); ?>">Talk to Us</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- hero contents end here -->
+        </div>
+        <!-- squre boxed content -->
+        <div class="hero-sqaure-box">
+            <div class="square-contents">
+                <div class="sqaure-title">
+                    <img src="http://localhost/projects/techventuras/wp-content/uploads/2022/03/done_all_24px.png"
+                        alt="">
+                    <h5 class="sqaure-title-h5">Discover the most effective digitization for your company.</h5>
+                </div>
+                <div class="sqaure-paragraph">
+                    <p>Many organizations waste tremendious amount of resources during their digitization
+                        process by not having the right strategy.</p>
+                </div>
+                <div class="square-redline">
+                    <p>Try Our Digitization Discovery Program<img
+                            src="http://localhost/projects/techventuras/wp-content/uploads/2022/03/arrow_forward_24px_outlined.svg"
+                            alt=""></p>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- featured case study -->
+    <section class="featured-case-study-section">
+        <div class="container">
+            <!-- case study -->
+            <div class="subtitle-case-study">
+                <img src="http://localhost/projects/techventuras/wp-content/uploads/2022/03/check_circle_24px_outlined.svg"
+                    alt="">
+                <p class="subtitle-paragraph">
+                    Featured Case Study
+                </p>
+            </div>
+            <!-- end of 1st case study -->
+            <div class="featured-main-case-study">
+                <p class="featured-main-case-study-text">Tech Venturas helps World Vision to build a digital survey
+                    platform for education sector.</p>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="case-study-img">
+                        <img src="http://localhost/projects/techventuras/wp-content/uploads/2022/03/pict_large-1.jpg"
+                            alt="">
+                    </div>
+                    <div class="read-case-study">
+                        <div class="read-case-study-rectangle">
+                            <p class="read-case-study-para">Read Case Study <img
+                                    src="http://localhost/projects/techventuras/wp-content/uploads/2022/03/arrow_forward_24px_outlined-1.svg"
+                                    alt=""></p>
+                        </div>
+
+                    </div>
+                    <div class="case-study-paragrapgh">
+                        <p>How Tech Venturas helped World Vision to build an intelligent digital survey platform. How
+                            Tech
+                            Venturas helped World Vision to build an intelligent digital survey platform. How Tech
+                            Venturas
+                            helped World Vision to build an intelligent digital survey platform.</p>
+                    </div>
+
+                </div>
+                <div class="col-md-6">
+                    <div class="case-study-main-title">
+                        <p>Technologies that transform organizations. We got it all.</p>
+                    </div>
+                    <div class="case-study-paragrapgh">
+                        <p>How Tech Venturas helped World Vision to build an intelligent digital survey platform. How
+                            Tech
+                            Venturas helped World Vision to build an intelligent digital survey platform. How Tech
+                            Venturas
+                            helped World Vision to build an intelligent digital survey platform.</p>
+                    </div>
+                    <div class="row">
+                        <div class="col-6">
+
+                            <!-- tick paragraph -->
+                            <div class="case-study-list">
+                                <div class="case-study-list-tick">
+                                    <img src="http://localhost/projects/techventuras/wp-content/uploads/2022/03/Group-11.png"
+                                        alt="">
+                                </div>
+                                <div class="case-study-tick-paragraph">
+                                    <p>
+                                        Building product strategies with extensive research capabilities.
+                                    </p>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div class="col-6">
+                            <div class="case-study-list">
+                                <div class="case-study-list-tick">
+                                    <img src="http://localhost/projects/techventuras/wp-content/uploads/2022/03/Group-11.png"
+                                        alt="">
+                                </div>
+                                <div class="case-study-tick-paragraph">
+                                    <p>
+                                        Tech Venturas helps World Vision to build an intelligent digital survey
+                                        platform.
+                                    </p>
+                                </div>
+
+                            </div>
+                        </div>
+
+                        <div class="col-6">
+                            <div class="case-study-list">
+                                <div class="case-study-list-tick">
+                                    <img src="http://localhost/projects/techventuras/wp-content/uploads/2022/03/Group-11.png"
+                                        alt="">
+                                </div>
+                                <div class="case-study-tick-paragraph">
+
+                                    <p>
+                                        Discovery in a box with idea validation in line with the solution in mind.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-6">
+                            <div class="case-study-list">
+                                <div class="case-study-list-tick">
+                                    <img src="http://localhost/projects/techventuras/wp-content/uploads/2022/03/Group-11.png"
+                                        alt="">
+                                </div>
+                                <div class="case-study-tick-paragraph">
+                                    <p>
+                                        Building proof-of-concept with pragmatics product development approach.
+                                    </p>
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="case-study-redline">
+                        <p>Find out why our approach is more effective<img
+                                src="http://localhost/projects/techventuras/wp-content/uploads/2022/03/arrow_forward_24px_outlined.svg"
+                                alt=""></p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <!-- creating-oppertunities -->
+    <section class="creating-oppertunities">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="title-creating-oppertunities">
+                        <h2 class="heading-creating-oppertunities text-center">
+                            Creating oppertunities through value
+                        </h2>
+                    </div>
+                    <div class="text-creating-oppertunities">
+                        <p class="text-center paragraph-creating-oppertunities">How Tech Venturas helped World Vision to
+                            build an intelligent digital survey platform. How
+                            Tech Venturas helped World Vision to build an intelligent digital survey platform. How Tech
+                            Venturas helped World Vision to build an intelligent digital survey platform.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Product Spotlight -->
+    <section class="product-spotlight">
+        <div class="spotlight-container">
+            <div class="row">
+                <!-- left side -->
+                <div class="col-md-6">
+                    <div class="spotlight-wrapper"
+                        style="background-image: url('http://localhost/projects/techventuras/wp-content/uploads/2022/03/Rectangle-9.png')">
+
+                        <div class="spotlight-sub-heading">
+                            <img src="http://localhost/projects/techventuras/wp-content/uploads/2022/03/Group-11.png"
+                                alt="">
+                            <p class="spotlight-sub-text">
+                                Product spotlight
+                            </p>
+                        </div>
+                        <!-- end of 1st case study -->
+                        <div class="spotlight-heading">
+                            <h2 class="spotlight-heading-enterprise">
+                                Enterprise <span class="red-word-spotlight">identity</span> governance and
+                                administration
+                                (IGA).
+                            </h2>
+                        </div>
+
+                        <div class="spotlight-paragraph">
+                            <p class="spotlight-paragraph">
+                                How Tech Venturas helped World Vision to build an intelligent digital survey platform.
+                                How
+                                Tech Venturas helped.
+                            </p>
+                        </div>
+                        <div class="row">
+                            <div class="col-6">
+
+                                <!-- tick paragraph -->
+                                <div class="case-study-list">
+                                    <div class="case-study-list-tick">
+                                        <img src="http://localhost/projects/techventuras/wp-content/uploads/2022/03/repeat_24px.png"
+                                            alt="">
+                                    </div>
+                                    <div class="case-study-tick-paragraph">
+                                        <p>
+                                            Building product strategies with extensive research capabilities.
+                                        </p>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div class="col-6">
+                                <div class="case-study-list">
+                                    <div class="case-study-list-tick">
+                                        <img src="http://localhost/projects/techventuras/wp-content/uploads/2022/03/games_24px.png"
+                                            alt="">
+                                    </div>
+                                    <div class="case-study-tick-paragraph">
+                                        <p>
+                                            Tech Venturas helps World Vision to build an intelligent digital survey
+                                            platform.
+                                        </p>
+                                    </div>
+
+                                </div>
+                            </div>
+
+                            <div class="col-6">
+                                <div class="case-study-list">
+                                    <div class="case-study-list-tick">
+                                        <img src="http://localhost/projects/techventuras/wp-content/uploads/2022/03/offline_bolt_24px.png"
+                                            alt="">
+                                    </div>
+                                    <div class="case-study-tick-paragraph">
+
+                                        <p>
+                                            Discovery in a box with idea validation in line with the solution in mind.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-6">
+                                <div class="case-study-list">
+                                    <div class="case-study-list-tick">
+                                        <img src="http://localhost/projects/techventuras/wp-content/uploads/2022/03/cached_24px.png"
+                                            alt="">
+                                    </div>
+                                    <div class="case-study-tick-paragraph">
+                                        <p>
+                                            Building proof-of-concept with pragmatics product development approach.
+                                        </p>
+                                    </div>
+
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="button-wrapper">
+                            <div class="btn homepage-btn hvr-grow">
+                                <a href="<?php //the_field('header_button', 'option'); ?>">Talk to Us</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+
+
+
+
+                <div class="col-md-6">
+                    <div class="spotlight-big-image">
+                        <img src="" alt="">
+                    </div>
+                    <!-- squre boxed content -->
+                    <div class="spotlight-sqaure-box">
+                        <div class="spotlight-square-contents">
+                            <div class="spotlight-sqaure-title">
+                                <img src="http://localhost/projects/techventuras/wp-content/uploads/2022/03/done_all_24px.png"
+                                    alt="">
+                                <p class="spotlight-sqaure-paragrapgh">“How Tech Venturas helped World Vision to build
+                                    an intelligent digital survey platform. How Tech Venturas helped.”</p>
+                            </div>
+                            <div class="spotlight-sqaure-ceo-name">
+                                <p>Roger Cruize</p>
+                            </div>
+                            <div class="spotlight-sqaure-ceo">
+                                <p>CIO - Accuity Enterprises</p>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
+
+
+
+
+</div>
+
+<?php
+get_footer();
